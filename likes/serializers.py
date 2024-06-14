bd3568b7a2a3d6fa8b9c5bdfb6e3724340d05ca1
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-06-14 10:22:11 +0000

    Adding extra fields to Posts ans Profiles part 2

--- likes/serializers.py
+++ likes/serializers.py
@@ -10,3 +10,11 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'created_at', 'owner', 'post'
         ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
