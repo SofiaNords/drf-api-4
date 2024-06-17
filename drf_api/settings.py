8642d3ac28a8de5431528dc4a34276214e0f3b9b
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-06-17 13:57:40 +0000

    Allowed hosts again

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -40,7 +40,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
-    'localhost',
+    '8000-sofianords-drfapi4-guvpya1jzih.ws.codeinstitute-ide.net',
     ]
 
 
